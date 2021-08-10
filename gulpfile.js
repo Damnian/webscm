0f559733e81f1db46ede790912361284ff73b9b1
Damnian <lewandowski.d90@gmail.com>
2021-08-10 15:19:45 +0200

    boostrap changes v2

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,13 @@ function copyHtml() {
 		.pipe(browserSync.stream());
 };
 
+function copyJs() {
+	return gulp.src('src/assets/scripts/*.js')
+		.pipe(injectHTML())
+		.pipe(gulp.dest('build/assets/js'))
+		.pipe(browserSync.stream());
+};
+
 // function copyImg() {
 // 	return gulp.src('src/assets/images/*')
 // 		.pipe(gulp.dest('build/assets/images'))
@@ -41,20 +48,29 @@ function copyFonts() {
 		.pipe(browserSync.stream());
 };
 
+function compressSvg() {
+	return gulp.src('src/assets/icons/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('build/assets/icons'))
+		.pipe(browserSync.stream());
+};
+
 
 function serve() {
 	browserSync({
 		server: 'build/'
 	});
 
-	gulp.watch(['src/assets/styles/**/*.scss', 'src/**/*.html'], gulp.series('buildStyles', 'copyHtml', 'copyFonts', 'compressImages'));
+	gulp.watch(['src/assets/styles/**/*.scss', 'src/**/*.html'], gulp.series('buildStyles', 'copyHtml', 'copyJs', 'copyFonts', 'compressImages', 'compressSvg'));
 	// gulp.watch(['src/assets/styles/**/*.scss', 'src/**/*.html'], gulp.series(reload));
 };
 
 
 exports.buildStyles = buildStyles;
 exports.copyHtml = copyHtml;
+exports.copyJs = copyJs;
 // exports.copyImg = copyImg;
 exports.serve = serve;
 exports.compressImages = compressImages;
 exports.copyFonts = copyFonts;
+exports.compressSvg = compressSvg;
